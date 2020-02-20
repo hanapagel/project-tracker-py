721a4f8575860ac687a5ca98e0d1cca1e91ffdf7
Your Name <you@example.com>
2020-02-20 09:55:34 -0800

    Further study

--- hackbright.py
+++ hackbright.py
@@ -69,15 +69,39 @@ def get_project_by_title(title):
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
+
+    QUERY = """SELECT student_github FROM grades WHERE student_github = :github
+                """
+
+    db_cursor = db.session.execute(QUERY, {'github': github})
+    new_row = db_cursor.fetchone()
+
+    if new_row is None:
+        print("Sorry, name not valid")
+        return
+
+    QUERY = """SELECT project_title FROM grades WHERE project_title = :title
+                """
+
+    db_cursor = db.session.execute(QUERY, {'title': title})
+    new_row = db_cursor.fetchone()
+
+    if new_row is None:
+        print("Sorry, title not valid")
+        return
+
     QUERY = """SELECT grade
                 FROM grades
                 WHERE  student_github = :github AND project_title = :title
                 """
     db_cursor = db.session.execute(QUERY, {'github': github, 'title': title})
-
+    print(db_cursor)
     new_row = db_cursor.fetchone()
-
-    print(f"Grade: {new_row[0]}")
+    print(new_row)
+    if new_row is None:
+        print("Student github or project title is not valid")
+    else:
+        print(f"Grade: {new_row[0]}")
 
 
 def assign_grade(github, title, grade):
@@ -91,6 +115,36 @@ def assign_grade(github, title, grade):
     db.session.commit()
 
     print("Success!")
+
+
+def add_project(title, description, max_grade):
+    """Create a new project entry and print a confirmation."""
+
+    QUERY = """ INSERT INTO projects (title, description, max_grade)
+                VALUES (:title, :description, :max_grade)
+                """
+
+    db.session.execute(QUERY, {'title': title, 'description': description,
+                               'max_grade': max_grade})
+    db.session.commit()
+
+    print("Success")
+
+
+def see_all_grades(github):
+    """Display project grades for students."""
+
+    QUERY = """SELECT project_title, grade 
+                FROM grades
+                WHERE student_github = :github"""
+
+    cursor = db.session.execute(QUERY, {'github': github})
+
+    new_row = cursor.fetchall()
+
+    for item in new_row:
+        print(f"{item[0]}: {item[1]}")
+
 
 def handle_input():
     """Main loop.
@@ -115,6 +169,26 @@ def handle_input():
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
 
+        elif command == "project_title":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "grade_by_github_title":
+            github, title = args
+            get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+            github, title, grade = args
+            assign_grade(github, title, grade)
+
+        elif command == "add_project":
+            title = args[0]
+            grade = args[-1]
+            description = args[1:-1]
+            description = ' '.join(description)
+
+            add_project(title, description, grade)
+
         else:
             if command != "quit":
                 print("Invalid Entry. Try again.")
@@ -123,7 +197,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
